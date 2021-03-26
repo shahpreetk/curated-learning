d9b1c1b2860e19693b0a392ece5c12e3dc02bf0c
Preet Shah <shahpreetk@gmail.com>
2021-03-26 13:03:42 +0530

    readme updated and few blogs added

--- src/blogs_data.js
+++ src/blogs_data.js
@@ -8,4 +8,21 @@ export const blogs_data = [
       "https://www.digitalocean.com/community/tutorials/how-to-set-up-a-react-project-with-parcel",
     _blog_tags: "#javascript #react",
   },
+  {
+    _id: "2",
+    _blog_title: "Using React Icons",
+    _blog_description:
+      "This blog by Kevin Sims (Dev.to) helps you add React Icons.",
+    _blog_url: "https://dev.to/kevsmss/easiest-way-to-use-icons-in-react-h0o",
+    _blog_tags: "#npm #react #react-icons",
+  },
+  {
+    _id: "3",
+    _blog_title: "Getting started with Vue",
+    _blog_description:
+      "Vue is a modern JavaScript framework that provides useful facilities for progressive enhancement. MDN docs has a tutorial to get you started.",
+    _blog_url:
+      "https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Vue_getting_started",
+    _blog_tags: "#javascript #vue #vuejs",
+  },
 ];
